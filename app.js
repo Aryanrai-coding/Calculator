12fb2e9d1179608194c97add6eb2b0a38501a491
Aryan <aryanpravinrai3@gmail.com>
2024-01-02 14:51:58 +0530

    Add logic to the calculator using js

---
+++ app.js
@@ -0,0 +1,117 @@
+
+// functions for operation
+
+function add(a, b) {
+    return a + b
+};
+
+function sub(a, b) {
+    return a - b
+};
+
+function mul(a, b) {
+    return a * b
+};
+
+function div(a, b) {
+    return a / b
+};
+
+// variables for operation
+
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+// updating display with number and operator and 
+// storing them
+
+let display = document.querySelector('.display');
+let numButtons = document.querySelectorAll('.num');
+let operatorButtons = document.querySelectorAll('.operation');
+
+
+for (let numButton of numButtons) {
+    numButton.addEventListener('click', () => {
+        updateDisplay(numButton.textContent);
+    })
+
+}
+
+for (let operatorButton of operatorButtons) {
+    operatorButton.addEventListener('click', () => {
+        updateOperator(operatorButton.textContent);
+
+    })
+}
+
+
+// function for displaying and storing numbers 
+
+function updateDisplay(value) {
+    if (!operator) {
+        num1 += value
+        display.textContent = num1;
+    } else {
+        num2 += value;
+        display.textContent = num2;
+    }
+
+}
+// function for displaying and storing operator
+
+function updateOperator(value) {
+    operator = value;
+    display.textContent = num1 + ' ' + operator;
+}
+
+// function for calculating using all the variables
+
+function operate() {
+
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    let result = 0;
+
+    if (operator == '+') {
+        result = add(num1, num2);
+    } else if (operator == '-') {
+        result = sub(num1, num2);
+    } else if (operator == '*') {
+        result = mul(num1, num2);
+    } else if (operator == '/') {
+        if (num2 !== 0) {
+            result = div(num1, num2);
+        } else {
+            display.textContent = "Cannot divide by 0";
+            return;
+        }
+
+    };
+
+    display.textContent = result.toString();
+
+    num1 = result.toString();
+    num2 = '';
+    operator = '';
+}
+
+// running the operate function
+
+let equals = document.querySelector('#equals');
+
+equals.addEventListener('click', () => {
+    operate();
+})
+
+// clearing and resetting everthing on the display 
+
+let clearBtn = document.querySelector('.clear');
+
+clearBtn.addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    display.textContent = '0';
+})
